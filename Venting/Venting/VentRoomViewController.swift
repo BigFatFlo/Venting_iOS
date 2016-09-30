54129a85e95b10178e1f915d74f695f666444c8b
Florent Remis <florent.remis@gmail.com>
2016-09-30 22:42:24 +0200

    Created all views and segues

--- Venting/Venting/VentRoomViewController.swift
+++ Venting/Venting/VentRoomViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Venting
 //
-//  Created by Florent Remis on 28/09/2016.
+//  Created by Florent Remis on 30/09/2016.
 //  Copyright (c) 2016 Spersio. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class VentRoomViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
-
